e3bfa3fb5fd0a5c31211ca4e0213a08f0627aa9e
mathiasepc <mathiasepc@msn.com>
2025-02-05 10:03:41 +0100

    initial commit

---
+++ src/main/java/com/example/touristguideapigruppe/TouristGuideApiGruppeApplication.java
@@ -0,0 +1,13 @@
+package com.example.touristguideapigruppe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristGuideApiGruppeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristGuideApiGruppeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/touristguideapigruppe/TouristGuideApiGruppeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.touristguideapigruppe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuideApiGruppeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
