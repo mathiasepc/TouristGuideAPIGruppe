9c6aa95a1928503b90d180873015c1dd666dd199
lalo0001@stud.kea.dk <laerkeloenborg@MacBook-Air-tilhrende-Lrke.local>
2025-02-12 16:50:56 +0100

    index styling

--- src/main/java/com/example/touristguideapigruppe/repositories/TouristRepository.java
+++ src/main/java/com/example/touristguideapigruppe/repositories/TouristRepository.java
@@ -48,7 +48,14 @@ public class TouristRepository {
     private void populateAttractions(){
         attractions.add(new TouristAttraction("Tivoli","Det er en forlystelses parke på sjælland"));
         attractions.add(new TouristAttraction("Assistent kirkegården","Det er en kirkegård med mange kendte"));
-        attractions.add(new TouristAttraction("Bakken", "Det er en forlystelses parke"));
-        attractions.add(new TouristAttraction("Krongborg", "Det er et historisk slot"));
+        attractions.add(new TouristAttraction("Bakken", "unik forlystelsespark med en helt særlig historie - midt ude i naturen"));
+        attractions.add(new TouristAttraction("Kronborg", "Det er et historisk slot"));
+        attractions.add(new TouristAttraction("Zoologisk have", "Familievenlig park med en masse dyr"));
+        attractions.add(new TouristAttraction("Amalienborg", "Historisk slotsbygning, kongefamiliens bosted, vagtskifte hverdag kl. 12"));
+        attractions.add(new TouristAttraction("Den lille havfrue", "Verdenskendt havfrue beliggende ved Langelinie"));
+        attractions.add(new TouristAttraction("Rundetårn", "Kom op i Europas ældste fungerende observationsbygning"));
+        attractions.add(new TouristAttraction("SMK", "Statens museum for kunst rummer mere end 700 års kunst og kunsthistorie"));
+        attractions.add(new TouristAttraction("Glyptoteket", "Hop forbi og se en imponerende samling af antik kunst og skulpturer i smukke omgivelser"));
+        attractions.add(new TouristAttraction("Nyhavn", "Hyggelig lille gade, ved vandet med masser af cafe muligheder"));
     }
 }
