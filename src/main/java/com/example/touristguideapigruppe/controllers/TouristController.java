4dfce30b15d37f54e64f4f5e60a38e6e3525934d
mathiasepc <mathiasepc@msn.com>
2025-02-13 22:24:50 +0100

    fået add i navbar

--- src/main/java/com/example/touristguideapigruppe/controllers/TouristController.java
+++ src/main/java/com/example/touristguideapigruppe/controllers/TouristController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/attractions")
+@RequestMapping("attractions")
 public class TouristController {
     private final TouristService touristService;
 
