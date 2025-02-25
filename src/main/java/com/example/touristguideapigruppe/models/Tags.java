e1046ffe73c618e86afb4f44268260408b8c1846
laerkeloenborg <lalo0001@stud.kea.dk>
2025-02-25 12:31:05 +0100

    tags og by tilføjet

---
+++ src/main/java/com/example/touristguideapigruppe/models/Tags.java
@@ -0,0 +1,11 @@
+package com.example.touristguideapigruppe.models;
+
+public enum Tags {
+    BØRNEVENLIG,
+    GRATIS,
+    MUSEUM,
+    KUNST,
+    NATUR,
+    RESTAURANT,
+    UNDERHOLDNING
+}
--- src/main/java/com/example/touristguideapigruppe/models/TouristAttraction.java
+++ src/main/java/com/example/touristguideapigruppe/models/TouristAttraction.java
@@ -1,15 +1,21 @@
 package com.example.touristguideapigruppe.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TouristAttraction {
-    private String name,description;
+    private String name,description, city;
+    private List<Tags> tags;
 
     public TouristAttraction(){
 
     }
 
-    public TouristAttraction(String name, String description) {
+    public TouristAttraction(String name, String description, String city, List<Tags> tags) {
         this.name = name;
         this.description = description;
+        this.city = city;
+        this.tags = tags;
     }
 
     public String getName() {
@@ -26,5 +32,21 @@ public class TouristAttraction {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<Tags> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tags> tags) {
+        this.tags = tags;
     }
 }
