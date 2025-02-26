6109d3ece358323f388743709a702f3d6fdb8a7f
mathiasepc <mathiasepc@msn.com>
2025-02-26 16:39:56 +0100

    Tilføjet displayName på enum

--- src/main/java/com/example/touristguideapigruppe/models/Tags.java
+++ src/main/java/com/example/touristguideapigruppe/models/Tags.java
@@ -1,11 +1,22 @@
 package com.example.touristguideapigruppe.models;
 
 public enum Tags {
-    BØRNEVENLIG,
-    GRATIS,
-    MUSEUM,
-    KUNST,
-    NATUR,
-    RESTAURANT,
-    UNDERHOLDNING
+    BØRNEVENLIG("Børne venlig"),
+    GRATIS("Gratis"),
+    MUSEUM("Museum"),
+    KUNST("Kunst"),
+    NATUR("Natur"),
+    RESTAURANT("Restaurant"),
+    UNDERHOLDNING("Underholdning");
+
+
+    private String displayName;
+
+    Tags(String displayName){
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
