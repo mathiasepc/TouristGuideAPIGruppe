3cc831e98e95d507edc9cda5bd459a5012e3da05
MartinMD-Git <mada0008@stud.kea.dk>
2025-02-27 15:37:35 +0100

    Kontakt os

--- src/main/java/com/example/touristguideapigruppe/controllers/TouristController.java
+++ src/main/java/com/example/touristguideapigruppe/controllers/TouristController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
